d639a47ad3e285514ff8613a299049a3a4032c60
Victoria-Hodder <morethangrammar@gmail.com>
2019-07-11 12:31:19 +0200

    moved try... except... database connection from routes.py to run.py

--- run.py
+++ run.py
@@ -14,10 +14,6 @@ password = "learningsql"
 query='select e.exhibition_id, s.museum_id, e.museum_name museum, e.exhibition_title title, e.exhibition_subtitle subtitle, e.search_keywords keyword, e.description, e.exhibition_picture_url imgurl,    e.exhibition_picture_caption imgcaption, e.exhibition_duration_start statdate, e.exhibition_duration_end enddate FROM museum.exhibition e, museum.museumspace s WHERE e.museum_name = s.museum_name AND e.exhibition_duration_end > now() ORDER BY e.exhibition_duration_end ASC'
 keywords_query = 'select e.search_keywords keyword FROM museum.exhibition e, museum.museumspace s WHERE e.museum_name = s.museum_name AND e.exhibition_duration_end > now() ORDER BY e.exhibition_duration_end ASC'
 
-
-#this ("query") is SQL language
-
-
 try:
     conn = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'" % (host, dbname, username, password) )
     cur = conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
@@ -31,6 +27,7 @@ try:
 
 except:
     print("I am unable to connect to the database")
+
 
 
 if __name__ == "__main__":
